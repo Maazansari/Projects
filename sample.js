7a70101ed66ab05c47ef5c0be7e627f19fef654d
Maazansari <k142203@nu.edu.pk>
2020-01-29 17:46:53 +0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,8 +1,4 @@
-const data = [
-  {
-    url: "https://web.facebook.com/?_rdc=1&_rdr",
-    requirments: [
-      "Enter a combination of at least six numbers, letters and punctuation marks (such as ! and &). "
-    ]
-  }
-];
+ [{
+ 	"url": "https://web.facebook.com/?_rdc=1&_rdr",
+ 	"requirments": ["Enteracombinationofatleastsixnumbers", "lettersandpunctuationmarks(suchas!and&)"]
+ }]
