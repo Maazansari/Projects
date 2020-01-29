b3d2e7193bb509d12a1a485ea51fac1c8cf30741
Maazansari <k142203@nu.edu.pk>
2020-01-29 18:47:17 +0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,4 @@
  [{
- 	"url": "https://web.facebook.com/?_rdc=1&_rdr",
+ 	"url": "https://www.google.com.pk/",
  	"requirments": ["Enteracombination of atleast six numbers", "letters and punctuation marks (suchas!and&)"]
  }]
