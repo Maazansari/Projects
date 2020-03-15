dab8253b55451afc82554055006dfbd3c3a5c615
Maazansari <k142203@nu.edu.pk>
2020-03-16 01:40:17 +0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,1 @@
- [{
- 	"url": "https://www.google.com.pk/",
- 	"requirments": ["Enteracombination of atleast six numbers", "letters and punctuation marks (suchas!and&)"]
- }]
+"AND Cnn.com OR Nytimes.com OR News.google.com OR Theguardian.com OR Washingtonpost.com OR News.yahoo.com OR Cnbc.com OR Bloomberg.com OR Reuters.com OR Usatoday.com OR Nbcnews.com OR Usnews.com OR Time.com OR Abcnews.go.com OR Thehill.com"
