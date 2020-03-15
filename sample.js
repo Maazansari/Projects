edbf87ba3b4d0251df7c6af1caff1ac3a0d17dac
Maazansari <k142203@nu.edu.pk>
2020-03-16 02:00:39 +0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,17 +1,37 @@
-[
-  "AND Cnn.com",
-  "OR Nytimes.com",
-  "OR News.google.com",
-  "OR Theguardian.com",
-  "OR Washingtonpost.com",
-  "OR News.yahoo.com",
-  "OR Cnbc.com",
-  "OR Bloomberg.com",
-  "OR Reuters.com",
-  "OR Usatoday.com",
-  "OR Nbcnews.com",
-  "OR Usnews.com",
-  "OR Time.com",
-  "OR Abcnews.go.com",
-  "OR Thehill.com"
-]
+{
+
+	"leftWhiteList": [
+		"AND Cnn.com",
+		"OR Nytimes.com",
+		"OR News.google.com",
+		"OR Theguardian.com",
+		"OR Washingtonpost.com",
+		"OR News.yahoo.com",
+		"OR Cnbc.com",
+		"OR Bloomberg.com",
+		"OR Reuters.com",
+		"OR Usatoday.com",
+		"OR Nbcnews.com",
+		"OR Usnews.com",
+		"OR Time.com",
+		"OR Abcnews.go.com",
+		"OR Thehill.com"
+	],
+	"rightWhiteList": [
+		"AND foxnews.com",
+		"OR wsj.com",
+		"OR rushlimbaugh.com",
+		"OR drudgereport.com",
+		"OR theblaze.com",
+		"OR dailymail.co.uk",
+		"OR forbes.com",
+		"OR washingtontimes.com",
+		"OR oann.com",
+		"OR cspan.com",
+		"OR mcclatchy.com",
+		"OR nypost.com",
+		"OR wnd.com",
+		"OR pjmedia.com",
+		"OR newsbusters.org"
+	]
+}
